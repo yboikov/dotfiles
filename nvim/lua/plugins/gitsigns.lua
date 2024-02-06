ffa89bc02677c2e0d3a7a2dbbffa7f6be44688b6
Yordan Boykov <y.boikov@gmail.com>
2024-02-06 21:28:50 +0200

    Add tpope/vim-fugitive plugin

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,6 +1,12 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	config = function()
-		require("gitsigns").setup()
-	end,
+	{
+		"tpope/vim-fugitive",
+	},
+
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end,
+	},
 }
