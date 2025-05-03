c2dcdfcda1da15f75f9592938c01d602de7591ec
Yordan Boykov <y.boikov@gmail.com>
2025-05-04 02:15:24 +0300

    change

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -6,38 +6,36 @@ vim.keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>", { desc = "Prev bu
 
 -- telescope
 vim.keymap.set("n", "<leader>sf", function()
-  require("telescope.builtin").find_files()
+    require("telescope.builtin").find_files()
 end, { desc = "Find files" })
 
 vim.keymap.set("n", "<leader>sg", function()
-  require("telescope.builtin").live_grep()
+    require("telescope.builtin").live_grep()
 end, { desc = "Live grep" })
 
 vim.keymap.set("n", "<leader>sb", function()
-  require("telescope.builtin").buffers()
+    require("telescope.builtin").buffers()
 end, { desc = "Find buffers" })
 
 vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
 
-        vim.keymap.set("n", "<leader>/", function()
-  builtin = require("telescope.builtin")
-            builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-                winblend = 10,
-                previewer = false,
-            }))
-        end, { desc = "[/] Fuzzily search in current buffer" })
-        -- It's also possible to pass additional configuration options.
-        --  See `:help telescope.builtin.live_grep()` for information about particular keys
-        vim.keymap.set("n", "<leader>s/", function()
-  builtin = require("telescope.builtin")
-            builtin.live_grep({
-                grep_open_files = true,
-                prompt_title = "Live Grep in Open Files",
-            })
-        end, { desc = "[S]earch [/] in Open Files" })
+vim.keymap.set("n", "<leader>/", function()
+    builtin = require("telescope.builtin")
+    builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+        winblend = 10,
+        previewer = false,
+    }))
+end, { desc = "[/] Fuzzily search in current buffer" })
+vim.keymap.set("n", "<leader>s/", function()
+    builtin = require("telescope.builtin")
+    builtin.live_grep({
+        grep_open_files = true,
+        prompt_title = "Live Grep in Open Files",
+    })
+end, { desc = "[S]earch [/] in Open Files" })
 
-        -- Shortcut for searching your Neovim configuration files
-        vim.keymap.set("n", "<leader>sn", function()
-  builtin = require("telescope.builtin")
-            builtin.find_files({ cwd = vim.fn.stdpath("config") })
-        end, { desc = "[S]earch [N]eovim files" })
+-- Shortcut for searching your Neovim configuration files
+vim.keymap.set("n", "<leader>sn", function()
+    builtin = require("telescope.builtin")
+    builtin.find_files({ cwd = vim.fn.stdpath("config") })
+end, { desc = "[S]earch [N]eovim files" })
